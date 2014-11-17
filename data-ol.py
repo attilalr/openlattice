297a45150bae9264e06bd93c6dcd137233b08776
Áttila Leães Rodrigues <attila.leaes@gmail.com>
2014-11-17 13:47:02 -0200

    change in data-ol.py, generate more fancy data

--- data-ol.py
+++ data-ol.py
@@ -1,7 +1,9 @@
-import random
+import random,math
 
 SIZELATTICE=100
 STEPS=10000
 
-for i in range(SIZELATTICE*STEPS):
-  print "%d %d %d" % (random.randrange(0,SIZELATTICE), random.randrange(0,SIZELATTICE), random.randrange(0,3))
+for k in range(0,STEPS):
+  for i in range(0,SIZELATTICE):
+    for j in range(0,SIZELATTICE):
+      print "%d %d %d" % (i,j, int(math.sin(i+j+k)+2)-int(math.sin(i+j-k)+1))
