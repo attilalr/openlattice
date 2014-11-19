0b1e643b8697b70013f80d6e53ee1a092a87d9ee
Áttila Leães Rodrigues <attila.leaes@gmail.com>
2014-11-19 15:37:39 -0200

    hope solved the upscale issue

--- data-ol.py
+++ data-ol.py
@@ -6,4 +6,5 @@ STEPS=10000
 for k in range(0,STEPS):
   for i in range(0,SIZELATTICE):
     for j in range(0,SIZELATTICE):
-      print "%d %d %d" % (i,j, int(math.sin(i+j+k)+2)-int(math.sin(i+j-k)+1))
+      print "%d %d %d" % (i,j, k%3)
+#      print "%d %d %d" % (i,j, int(math.sin(i+j+k)+2)-int(math.sin(i+j-k)+1))
